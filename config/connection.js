add0bf528ce3b2826cf7d71e376040db4cc971e8
Carina Reyes <59748133+Creyes17e@users.noreply.github.com>
2020-07-14 22:35:39 -0500

    changed connection for heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -5,11 +5,11 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   var connection = mysql.createConnection({
-    host: "zf4nk2bcqjvif4in.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    host: "localhost",
     port: 3306,
-    user: "vosfh3e1d9r6n2q2",
-    password: "kpmrpow2c68xgy4v",
-    database: "ihp9pzq04bdjr50w",
+    user: "root",
+    password: "password",
+    database: "burgers_db",
   });
 }
 
